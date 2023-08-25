4c2fe1b5f1d2b6b9b9e702e369b21e3a67ef428c
theSonOfBen <58787394+thesonofben@users.noreply.github.com>
2023-08-25 17:33:58 +0000

    Update 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,26 @@ function App() {
   const [userEmail, setUserEmail] = useState<string | undefined>(
     "unknown user"
   );
-
+  const [CustomerEmail, setCustomerEmail] = useState<string | undefined>(
+    "unknown user"
+  );
   const [status, setStatus] = useState<string | undefined>("unknown status");
+  const [fetchedData, setFetchedData] = useState<string | undefined>();
 
-  const {user, conversation} = useHelpScoutContext()
+  const { user, conversation, customer } = useHelpScoutContext();
+
   useEffect(() => {
-    setUserEmail(user?.email)
-    setStatus(conversation?.status)
-  }, [user, conversation]);
+    setUserEmail(user?.firstName);
+    setCustomerEmail(customer?.emails[0]?.value);
+    setStatus(conversation?.status);
+
+    if (CustomerEmail) {
+      // Check if CustomerEmail is not undefined
+      fetchData(CustomerEmail).then((data) => {
+        setFetchedData(data.country);
+      });
+    }
+  }, [user, conversation, customer]);
 
   function onClick() {
     HelpScout.showNotification(
@@ -34,9 +46,12 @@ function App() {
   return (
     <div className="App" ref={appRef}>
       <DefaultStyle />
-      <Heading level="h1">Hi, {userEmail}</Heading>
-      <Text>The conversation is {status}</Text>
-      <br />
+      <Heading level="h1">Hello, {userEmail} </Heading>
+      <Text>
+        The conversation is {status} for the customer with the email{" "}
+        {CustomerEmail} --- {fetchedData}--- {goegr()}
+      </Text>
+
       <Button size="sm" onClick={onClick}>
         Click me
       </Button>
@@ -45,3 +60,34 @@ function App() {
 }
 
 export default App;
+
+async function fetchData(customerEmail: string) {
+  try {
+    const options = {
+      method: "post",
+      //followRedirects: true,
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      body: JSON.stringify({ data: customerEmail }), // Send data as JSON string
+    };
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbzauu8KHNjvzw4oH2ZNOEBEnBigNjuCAsNoByRALukyFgLjRLGev7xt1_w0tY5WnJF-/exec",
+      options
+    );
+    if (!response.ok) {
+      throw new Error("Response not OK");
+    }
+
+      const jsonData = await response.json()
+    return jsonData;
+  } catch (error) {
+    return "error: " + error;
+  }
+}
+
+//fetchData();
+
+function goegr() {
+  return "hello";
+}
